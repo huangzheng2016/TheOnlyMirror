4bd0afb36d2a341070ea439953c3dd40c1de063a
黄正 <huangzheng.0e7@bytedance.com>
2024-07-09 01:44:58 +0800

    fix pypi gzip format data

--- plugins/reverse.go
+++ plugins/reverse.go
@@ -2,7 +2,10 @@ package plugins
 
 import (
 	"TheOnlyMirror/config"
+	"bytes"
+	"compress/gzip"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
@@ -25,7 +28,20 @@ func HandlerReverse(w http.ResponseWriter, r *http.Request, source config.Source
 				return err
 			}
 			resp.Body.Close()
-			modifiedBody := string(bodyBytes)
+			encoding := resp.Header.Get("Content-Encoding")
+			var modifiedBody string
+			switch encoding {
+			case "gzip":
+				reader, _ := gzip.NewReader(bytes.NewBuffer(bodyBytes))
+				dataDecompressing, err := ioutil.ReadAll(reader)
+				if err != nil {
+					modifiedBody = string(bodyBytes)
+				} else {
+					modifiedBody = string(dataDecompressing)
+				}
+			default:
+				modifiedBody = string(bodyBytes)
+			}
 			for _, replace := range source.Replaces {
 				src := replace.Src
 				dst := replace.Dst
@@ -40,6 +56,14 @@ func HandlerReverse(w http.ResponseWriter, r *http.Request, source config.Source
 					}
 				}
 				modifiedBody = strings.Replace(modifiedBody, src, dst, -1)
+			}
+			switch encoding {
+			case "gzip":
+				var b bytes.Buffer
+				gw := gzip.NewWriter(&b)
+				_, _ = gw.Write([]byte(modifiedBody))
+				_ = gw.Close()
+				modifiedBody = string(b.Bytes())
 			}
 			resp.Body = io.NopCloser(strings.NewReader(modifiedBody))
 			resp.ContentLength = int64(len(modifiedBody))
